8bfd76b6ccf64a5c003662d7ada3f35dcea4ce43
Andrew Fahmy <fahmyand@utccgl.com>
2020-05-30 22:53:17 +0200

    version 1.0.0

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react';
+import logo from './images/logo.svg';
+import './app.scss';
+
+
+function App(): ReactElement {
+    return (
+        <header className="app-contents">
+            <img src={logo} className="spinner-logo" alt="react logo" />
+            <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+            <a href="https://reactjs.org" rel="noreferrer" target="_blank">Learn React</a>
+        </header>
+    );
+}
+
+
+export default App;
